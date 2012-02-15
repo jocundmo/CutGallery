598ab34711e801c22e84f5b59547aef7ce4575c6
Jimmy Mo <jocund_mo@yahoo.com.cn>
2012-02-15 22:58:51 +0800

    Fix bug - In some browser like FireFox, it always show full url like 'localhost/gallery3/index.php'. And location.href, href is a must in in Firefox,

--- modules/gallery/views/login_successful.html.php
+++ modules/gallery/views/login_successful.html.php
@@ -1,6 +1,13 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 <div>
-<?= t("Page is navigating... Login is successful... Please wait...") ?>
-  <script>window.location="index.php/albums";</script>
+<?= t("Page is navigating... <br/> Login is successful... <br/>Please wait...<br/><a href=>Click</a> if your browser is not refreshed...") ?>
+  <script>
+      if (location.href.toLocaleString().indexOf("index.php", 0)> 0){
+        location.href="albums";
+        }
+      else{
+        location.href="index.php/albums";
+        }
+  </script>
 </div>
 
