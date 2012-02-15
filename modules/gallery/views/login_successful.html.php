8d666e658f8f74d1c96ea0cf37077410bb5ea41a
Jimmy Mo <jocund_mo@yahoo.com.cn>
2012-02-15 21:51:42 +0800

    Fix bug - auto navigate should include "index.php", otherwise, a 404 page not found will throw.

--- modules/gallery/views/login_successful.html.php
+++ modules/gallery/views/login_successful.html.php
@@ -1,6 +1,6 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 <div>
 <?= t("Page is navigating... Login is successful... Please wait...") ?>
-  <script>window.location="albums";</script>
+  <script>window.location="index.php/albums";</script>
 </div>
 
