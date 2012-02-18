e8a919f4a5586c2d7c06b704d972cee7ebb68d1c
Jimmy Mo <jocund_mo@yahoo.com.cn>
2012-02-18 23:05:13 +0800

    Function Added - Add couple series of home pics and use rand to show them.

--- themes/wind/views/home.html.php
+++ themes/wind/views/home.html.php
@@ -1,8 +1,9 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 <div id="home_pictures">
     <ul  class="Slides" id="Slides">
-        <li><img style="width: 275px; height: 500px;"src="<?= url::file("themes/wind/images/home-pic1.png") ?>"/></li>
-        <li><img style="width: 275px; height: 500px;"src="<?= url::file("themes/wind/images/home-pic2.png") ?>"/></li>
-        <li><img style="width: 275px; height: 500px;"src="<?= url::file("themes/wind/images/home-pic3.jpg") ?>"/></li>
+        <? $index = rand(0, 2)?>
+        <li><img style="width: 275px; height: 500px;"src="<?= url::file("themes/wind/homepic/site$index/home-pic1.png") ?>"/></li>
+        <li><img style="width: 275px; height: 500px;"src="<?= url::file("themes/wind/homepic/site$index/home-pic2.png") ?>"/></li>
+        <li><img style="width: 275px; height: 500px;"src="<?= url::file("themes/wind/homepic/site$index/home-pic3.png") ?>"/></li>
     </ul>
 </div>
