8238f6b8e60022f8c72ce88af0e5a29b2e3a8687
Jimmy Mo <jocund_mo@yahoo.com.cn>
2012-02-18 22:41:17 +0800

    Bug fix - Mainly for adding the home 3 pics.

--- themes/wind/views/home.html.php
+++ themes/wind/views/home.html.php
@@ -1,8 +1,8 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 <div id="home_pictures">
-    <ul>
-        <li>pic1</li>
-        <li>pic2</li>
-        <li>Pic3</li>
+    <ul  class="Slides" id="Slides">
+        <li><img style="width: 275px; height: 500px;"src="<?= url::file("themes/wind/images/home-pic1.png") ?>"/></li>
+        <li><img style="width: 275px; height: 500px;"src="<?= url::file("themes/wind/images/home-pic2.png") ?>"/></li>
+        <li><img style="width: 275px; height: 500px;"src="<?= url::file("themes/wind/images/home-pic3.jpg") ?>"/></li>
     </ul>
 </div>
