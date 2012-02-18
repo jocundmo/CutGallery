5bd9ecb5085f6d68c55bea3037f51f26834847e9
Jimmy Mo <jocund_mo@yahoo.com.cn>
2012-02-19 00:08:11 +0800

    Fix bug - Adjust the size of the homepic

--- themes/wind/views/home.html.php
+++ themes/wind/views/home.html.php
@@ -2,8 +2,8 @@
 <div id="home_pictures">
     <ul  class="Slides" id="Slides">
         <? $index = rand(0, 2)?>
-        <li><img style="width: 275px; height: 500px;"src="<?= url::file("themes/wind/homepic/site$index/home-pic1.png") ?>"/></li>
-        <li><img style="width: 275px; height: 500px;"src="<?= url::file("themes/wind/homepic/site$index/home-pic2.png") ?>"/></li>
-        <li><img style="width: 275px; height: 500px;"src="<?= url::file("themes/wind/homepic/site$index/home-pic3.png") ?>"/></li>
+        <li><img style="width: 318px; height: 510px;"src="<?= url::file("themes/wind/homepic/site$index/home-pic1.png") ?>"/></li>
+        <li><img style="width: 318px; height: 510px;"src="<?= url::file("themes/wind/homepic/site$index/home-pic2.png") ?>"/></li>
+        <li><img style="width: 318px; height: 510px;"src="<?= url::file("themes/wind/homepic/site$index/home-pic3.png") ?>"/></li>
     </ul>
 </div>
