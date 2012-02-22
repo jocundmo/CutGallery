31a1f57763af374f47d4ff00fec68a1cf7b3e966
unknown <JimmyMo@HSHAXPLT33MP8R.(none)>
2012-02-22 17:07:05 +0800

    Enhance: replace the home pic with smaller ones.

--- themes/wind/views/home.html.php
+++ themes/wind/views/home.html.php
@@ -1,7 +1,7 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 <div id="home_pictures">
     <ul  class="Slides" id="Slides">
-        <? $index = rand(0, 3); ?>
+        <? $index = rand(0, 4); ?>
         <li><img style="width: 318px; height: 510px;"src="<?= url::file("themes/wind/homepic/site$index/home-pic1.png") ?>"/></li>
         <li><img style="width: 318px; height: 510px;"src="<?= url::file("themes/wind/homepic/site$index/home-pic2.png") ?>"/></li>
         <li><img style="width: 318px; height: 510px;"src="<?= url::file("themes/wind/homepic/site$index/home-pic3.png") ?>"/></li>
