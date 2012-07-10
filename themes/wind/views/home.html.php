3fe8228fb71089cae1f5853930b28b98fe92a053
lex4azeroth <mxhray@hotmail.com>
2012-07-10 23:18:52 +0800

    Three pages
    Home
    AboutUs
    ContactUs

--- themes/wind/views/home.html.php
+++ themes/wind/views/home.html.php
@@ -1,4 +1,34 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
+<div id="home_pictures" class="ui-corner-all">
+    <ul>
+        <li><img src="<?= url::file("themes/wind/homepic/home-pic1.jpg") ?>"/></li>
+        <li><img src="<?= url::file("themes/wind/homepic/home-pic2.jpg") ?>"/></li>
+    </ul>
+    <ul>
+        <li><img src="<?= url::file("themes/wind/homepic/home-pic3.jpg") ?>"/></li>
+        <li><img src="<?= url::file("themes/wind/homepic/home-pic4.jpg") ?>"/></li>
+    </ul>
+<!--<table id="home_table" >
+    <tr>
+        <td>
+            <img src="<?= url::file("themes/wind/homepic/site0/home-pic1.jpg") ?>"/>
+        </td>
+        <td>
+            <img src="<?= url::file("themes/wind/homepic/site0/home-pic2.jpg") ?>"/>
+        </td>
+        
+    </tr>
+    <tr>
+        <td>
+            <img src="<?= url::file("themes/wind/homepic/site0/home-pic3.jpg") ?>"/>
+        </td>
+        <td>
+            <img src="<?= url::file("themes/wind/homepic/site0/home-pic1.jpg") ?>"/>
+        </td>
+    </tr>
+</table>-->
+</div>
+<!--   -moz-border-radius: 5px; -webkit-border-radius: 5px; border-radius: 5px;
 <div id="home_pictures">
     <ul  class="Slides" id="Slides">
         <? $index = rand(0, 4); ?>
@@ -7,3 +37,4 @@
         <li><img style="width: 318px; height: 510px;"src="<?= url::file("themes/wind/homepic/site$index/home-pic3.jpg") ?>"/></li>
     </ul>
 </div>
+-->
